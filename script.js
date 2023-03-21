559941305f6a18e44fb2034531b5eac58739229c
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-03-21 23:39:10 +0500

    if practice

--- script.js
+++ script.js
@@ -53,14 +53,45 @@ console.log("Result of a1 ", a1)
 
 // convert data string into number data (=+) is used for change data
 
-var numberEnter =+ prompt("Enter first number")
+// var numberEnter =+ prompt("Enter first number")
 
-var x2 = numberEnter==4 && 6>5
-console.log("Result of x2 ", x2)
+// var x2 = numberEnter==4 && 6>5
+// console.log("Result of x2 ", x2)
 
 
 
-var numberEnter2 =+ prompt("Enter second number")
+// var numberEnter2 =+ prompt("Enter second number")
 
-var x3 = numberEnter+numberEnter2==6 || 6>7 && x2
-console.log("Result of x3 ", x3)
+// var x3 = numberEnter+numberEnter2==6 || 6>7 && x2
+// console.log("Result of x3 ", x3)
+
+
+// If program
+// write a program that prompt the user to enter a number and check if it positive number,
+// display the result inan alert box.
+
+var p =+ prompt("Enter a number");
+
+if(p>=0){
+    console.log("You enter a postive number");
+}
+
+
+if(p<0){
+    console.log("You enter a negative number");
+}
+
+// write a program that prompts the user to entertwo numbers and check if
+// they are equal. Display the result in an alert box.
+
+var e =+ prompt("Enter fisrt number")
+var f =+ prompt("Enter a second number")
+
+if(e==f){
+    alert("Numbers are equals");
+}
+
+if(e!=f){
+    alert("Numbers are not equals")
+}
+
